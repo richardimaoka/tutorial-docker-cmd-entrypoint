9fb2c6fb12d79ae1f4b8081a67c9fd31eb04e625
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-09-02 22:52:56 +0900

    more description in commands

--- commands.py
+++ commands.py
@@ -8,42 +8,63 @@ def dockerfile(filename):
     print(str)
 
 
-def template(suffix, category):
+def docker_commands(suffix, category):
     str = """
+
+以下のコマンドを実行してください。
+
 ```sh
-docker build - t {suffix} - f Dockerfile.{suffix} .
-docker images - -filter = reference = '{category}*'
+docker build -t {suffix} -f Dockerfile.{suffix} .
 docker run {suffix}
-docker inspect {suffix} - -format = 'CMD {{.Config.Cmd}}'
 ```
 """.format(suffix=suffix, category=category).strip() + "\n"
-
     print(str)
 
 
+def template(suffix, category, result=None):
+    dockerfile("Dockerfile." + suffix)
+    docker_commands(suffix, category)
+    if result is not None:
+        print("""
+```sh:docker runの結果        g
+{result}
+```
+        """.format(result=result).strip() + "\n")
+
+
 def main():
-    template("cmd1", "cmd")  # CMD ["echo", "abc"]
-    dockerfile("Dockerfile.cmd1")
+    template("cmd1", "cmd", "abc")  # CMD ["echo", "abc"]
     template("cmd2", "cmd")  # CMD ["cho", "abc", "def]
     template("cmd3", "cmd")  # CMD echo abc
     template("cmd4", "cmd")  # CMD echo abc def
     template("cmd5", "cmd")  # CMD echo "$HOME"
     template("cmd6", "cmd")  # CMD ["sh", "-c", "echo $HOME"]
     print("""
+```sh
 # SIGTERM not received with shell form
-docker run nginx: 1.21
-# Ctrl + c works
+docker run nginx:1.21
+```
 
+以下のショートカットを実行してください。
+
+```sh
+# Ctrl + c 
+```
+
+以下のコマンドを実行してください。
+
+````sh
 docker build - t nginx: cmd-shellform - f Dockerfile.cmd-nginx .
 docker run - -name cnt-nginx-shellform nginx: cmd-shellform
 # Ctrl + c, not working!!
+```
 
-# another terminal
+```sh:another terminal
 docker stop cnt-nginx-shellform
 """)
     template("cmd7", "cmd")  # CMD ["echo", "abc", "def]
-    template("cmd7", "cmd")  # CMD["tail", "-f", "/dev/null"]
-    template("cmd7", "cmd")  # CMD tail - f / dev/null
+    template("cmd8", "cmd")  # CMD["tail", "-f", "/dev/null"]
+    template("cmd9", "cmd")  # CMD tail - f / dev/null
 
 
 if __name__ == "__main__":
